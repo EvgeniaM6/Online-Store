de3280bfd6b31ba5b978040e76291d0bc9720356
EvgeniaM6 <evgminukova6@gmail.com>
2022-12-21 23:18:06 +0200

    refactor: change name for models folder

---
+++ src/models/types.ts
@@ -0,0 +1,20 @@
+export interface IProducts {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
+
+export type DataGoods = {
+  products: IProducts[];
+  total: number;
+  skip: number;
+  limit: number;
+};
