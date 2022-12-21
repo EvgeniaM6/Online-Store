07eb35206cf8de30bf2639ed18a4113216e67b7d
EvgeniaM6 <evgminukova6@gmail.com>
2022-12-22 00:45:08 +0200

    refactor: add header.ts

---
+++ src/components/header/header.ts
@@ -0,0 +1,14 @@
+export default class Header {
+  constructor() {
+    console.log('header');
+  }
+
+  drawTemplate(): void {
+    // const headerEl = createEl
+    // this.cardTotalNum:
+  }
+
+  updateData(): void {
+    // this.cardTotalNum.textContent
+  }
+}
