271b01668cccdd358d3ebebc140688b8fbede14a
avshir <av.shirinskaya@gmail.com>
2022-12-30 20:44:12 +0100

    feat: add mainPageTemplate .ts and .scss

---
+++ src/components/mainPageTemplate/mainPageTemplate.ts
@@ -0,0 +1,50 @@
+import { CreateNode } from '../../utilities';
+import './mainPageTemplate.scss';
+
+export default class MainPageTemplate {
+  drawMainPageTemplate(): void {
+    const main = document.querySelector('.main') as HTMLElement;
+    const mainPageTemplate = new CreateNode(main, 'div', 'products container', '');
+    mainPageTemplate.node.innerHTML = this.mainTemplate();
+  }
+
+  mainTemplate(): string {
+    return `
+      <div class="products__filters">
+        <div class="reset-copy-url-block">
+          <button class="reset-btn btn">Reset filters</button>
+          <button class="copy-url-btn btn">Copy link</button>
+        </div>
+        <div class="filters-block">
+        </div>
+        <div class="dual-sliders-block">
+        </div>
+      </div>
+      <div class="products__main">
+        <div class="products__sorting-block">
+          <div class="sort-by">
+            <select class="sort-by__select input--big" name="" id="">
+              <option class="sort-name" value="sort-title" disabled selected>Sort options:</option>
+              <option value="price-ASC">Sort by price ASC</option>
+              <option value="price DESC">Sort by price DESC</option>
+              <option value="rating-ASC">Sort by rating ASC</option>
+              <option value="rating-DESC">Sort by rating DESC</option>
+              <option value="discount-ASC">Sort by discount ASC</option>
+              <option value="discount-DESC">Sort by discount DESC</option>
+            </select>
+          </div>
+          <div class="found-products">Found: <span>100</span></div>
+          <div class="search-bar">
+            <input class="search-filter input--big" type="search" placeholder="Search product..." />
+          </div>
+          <div class="view-cards">
+            <div class="view-card active view-card__1">view-1</div>
+            <div class="view-card view-card__2">view-2</div>
+          </div>
+        </div>
+        <div class="products__cards">
+        </div>
+      </div>
+    `;
+  }
+}
