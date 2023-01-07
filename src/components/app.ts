081bf121267433018b5a64b9d65488ec8d930d8d
EvgeniaM6 <evgminukova6@gmail.com>
2023-01-08 01:40:55 +0200

    feat: start dual sliders

--- src/components/app.ts
+++ src/components/app.ts
@@ -6,6 +6,7 @@ import DataBase from './dataBase';
 import MainPageTemplate from './mainPageTemplate/mainPageTemplate';
 import ProductCard from './productCard/productCard';
 import Filter from './filter/filter';
+import DualFilter from './dualFilter/dualSlider';
 
 export default class App {
   header: Header;
@@ -16,6 +17,7 @@ export default class App {
   mainPageTemplate: MainPageTemplate;
   productCard: ProductCard;
   filter: Filter;
+  dualFilter: DualFilter;
 
   constructor() {
     this.header = new Header();
@@ -26,6 +28,7 @@ export default class App {
     this.mainPageTemplate = new MainPageTemplate();
     this.productCard = new ProductCard();
     this.filter = new Filter();
+    this.dualFilter = new DualFilter();
   }
 
   start(): void {
--- src/components/dualFilter/dualSlider.ts
+++ src/components/dualFilter/dualSlider.ts
@@ -1,21 +1,31 @@
+import { Filters } from '../../models';
+import { createElem } from '../../utilities';
 import './dualFilter.scss';
 
-// const arrDualFilterType = ['price', 'stock'];
-export default function drawDualFilter(dualFilterType: string): string {
-  return `
-  <div class="dual-sliders-block">
-    <div class="dual-slider dual-slider-${dualFilterType}">
-      <div class="dual-slider__title">${dualFilterType}</div>
-      <div class="dual-slider__out-data">
-        <div class="dual-slider__from-data">$10.00</div>
-        <span>⟷</span>
-        <div class="dual-slider__to-data">$1100.00</div>
+export default class DualFilter {
+  createDualFilterElems(): void {
+    Object.values(Filters).forEach((filterType) => {
+      if (filterType.slice(0, 3) === 'ds:') {
+        const dualSlidersBlock = createElem('div', 'dual-sliders-block', '.dual-sliders');
+        dualSlidersBlock.innerHTML = this.drawDualFilter(filterType.slice(3));
+      }
+    });
+  }
+
+  drawDualFilter(dualFilterType: string): string {
+    return `
+      <div class="dual-slider dual-slider-${dualFilterType}">
+        <div class="dual-slider__title">${dualFilterType}</div>
+        <div class="dual-slider__out-data">
+          <div class="dual-slider__from-data">$10.00</div>
+          <span>⟷</span>
+          <div class="dual-slider__to-data">$1100.00</div>
+        </div>
+        <div class="dual-slider__control">
+          <input class="dual-slider__fromSlider" type="range" value="20" min="0" max="100" />
+          <input class="dual-slider__toSlider" type="range" value="80" min="0" max="100" />
+        </div>
       </div>
-      <div class="dual-slider__control">
-        <input class="dual-slider__fromSlider" type="range" value="20" min="0" max="100" />
-        <input class="dual-slider__toSlider" type="range" value="80" min="0" max="100" />
-      </div>
-    </div>
-  </div>
-  `;
+    `;
+  }
 }
