1cf2e7bfa4e06e266456692adad443211425917f
EvgeniaM6 <evgminukova6@gmail.com>
2023-01-09 16:38:49 +0200

    feat: connect dataBase, fix event listeners and fix product item drawing

--- src/components/basketPage/basketPage.ts
+++ src/components/basketPage/basketPage.ts
@@ -1,69 +1,51 @@
 import './basketPage.scss';
-import { IProducts } from '../../models/types';
-import { arrProductsInCart } from '../../data/testData'; //test data
 import ModalPayment from '../modalPayment/modalPayment';
+import { IProducts, IBasketProduct, IFiltersElems } from '../../models';
+import { createElem } from '../../utilities';
 
 const promoCodes: [string, number][] = [
   ['test', 10],
   ['RS', 25],
 ];
 export default class BasketPage {
+  productsItemNumElem: IFiltersElems = {};
+
   drawBasketPage(): void {
     const main = document.querySelector('.main') as HTMLElement;
     main.innerHTML = this.basketPageTemplate();
 
-    //show items from arrProductsInCart
-    let basketItems = '';
-    arrProductsInCart.forEach((obj, index) => {
-      basketItems += this.basketPageItemTemplate(obj, index);
-    });
+    const basket = this.getBasketProductsArr();
+    this.productsItemNumElem = {};
     const basketPageItems = document.querySelector('.basket-page__items') as HTMLElement;
-    basketPageItems.innerHTML = basketItems;
+
+    basket.forEach((basketProduct, index) => {
+      const id = basketProduct.product.id;
+      const price = basketProduct.product.price;
+      const basketItemBlock = createElem('div', 'basket-page__item');
+      basketItemBlock.innerHTML = this.basketPageItemTemplate(basketProduct.product, index, basketProduct.amount);
+      const keyBlock = `${id}_block`;
+      this.productsItemNumElem[keyBlock] = basketItemBlock;
+
+      const productAmountElem = basketItemBlock.querySelector('.basket-product-info__amount') as HTMLElement;
+      const keyAmountElem = `${id}_amount`;
+      this.productsItemNumElem[keyAmountElem] = productAmountElem;
+
+      const removeBtn = basketItemBlock.querySelector('#remove-product-btn') as HTMLButtonElement;
+      const addBtn = basketItemBlock.querySelector('#add-product-btn') as HTMLButtonElement;
+
+      removeBtn.addEventListener('click', (e) => this.addOrRemoveProduct(e, id, price));
+      addBtn.addEventListener('click', (e) => this.addOrRemoveProduct(e, id, price));
+
+      const currAmountSum = basketItemBlock.querySelector('.basket-product-info__amount-sum') as HTMLElement;
+      const keyTotal = `${id}_total`;
+      this.productsItemNumElem[keyTotal] = currAmountSum;
+
+      basketPageItems.append(basketItemBlock);
+    });
     this.summaryData();
 
     // ==========
     //add-remove countProduct(item) in cart, update for product amountSum
-    if (basketPageItems) {
-      basketPageItems.addEventListener('click', (e) => {
-        const currBtn = e.target as HTMLElement;
-        const currBasketPageItem = currBtn.closest('.basket-page__item');
-
-        const stockCurrProducts = currBasketPageItem?.querySelector('.basket-product-info__stock') as HTMLElement;
-        const valStockCurrProducts = Number(stockCurrProducts.textContent?.trim().slice(7));
-
-        const countCurrProducts = currBasketPageItem?.querySelector('.basket-product-info__amount') as HTMLElement;
-        let currCount = Number(countCurrProducts.textContent);
-
-        const currAmountSum = currBasketPageItem?.querySelector('.basket-product-info__amount-sum') as HTMLElement;
-        const currPriceElem = currBasketPageItem?.querySelector('.basket-product-info__price') as HTMLElement;
-        const currPrice = Number(currPriceElem.textContent?.trim().slice(7, -2));
-
-        if (currBtn.id === 'add-product-btn') {
-          if (currCount < valStockCurrProducts) {
-            currCount++;
-            countCurrProducts.textContent = `${currCount}`;
-          }
-        }
-
-        if (currBtn.id === 'remove-product-btn') {
-          if (currCount > 0) {
-            currCount--;
-            countCurrProducts.textContent = `${currCount}`;
-          }
-        }
-
-        //update data
-        currAmountSum.textContent = `Total: ${currPrice * currCount} $`;
-        if (currCount === 0) {
-          currBasketPageItem?.remove();
-          //TODO: удалить товар из arrТоваров корзины
-          //TODO: обновить порядковый номер товара в списке
-        }
-        //TODO: обновить данные о кол-ве товаров и цены в arrТоваров в Header
-        //TODO: обновить данные о кол-ве товаров и цены в arrТоваров в Summary
-        this.summaryData();
-      });
-    }
 
     //Summary block
     const summaryBasketPage = document.querySelector('.basket-page__summary') as HTMLElement;
@@ -75,6 +57,7 @@ export default class BasketPage {
         if (currSummaryBtn.id === 'basket-page__buy-now-btn') {
           const modalPayment = new ModalPayment();
           modalPayment.drawModalPayment();
+          console.log('promoCodes=', promoCodes);
         }
       });
     }
@@ -99,12 +82,71 @@ export default class BasketPage {
       const val = Number(el.textContent?.trim().slice(7, -2));
       sumTotalAmountSum += val;
     });
-    productsAmountStart.textContent = `Total: ${sumTotalAmountSum}$`;
+    productsAmountStart.textContent = `Total: ${sumTotalAmountSum}€`;
   }
 
-  basketPageItemTemplate(obj: IProducts, index: number): string {
+  getBasketProductsArr(): Array<IBasketProduct> {
+    return window.app.dataBase.getBasket();
+  }
+
+  addOrRemoveProduct(e: Event, id: number, price: number): void {
+    const currBtn = e.target as HTMLButtonElement;
+    const valStockCurrProducts = this.getProductStockById(id);
+    const countCurrProducts = this.productsItemNumElem[`${id}_amount`] as HTMLElement;
+    const currAmountSum = this.productsItemNumElem[`${id}_total`] as HTMLElement;
+    let currCount = countCurrProducts ? (countCurrProducts.textContent ? +countCurrProducts.textContent : 0) : 0;
+    let isPlus = false;
+
+    if (currBtn.id === 'add-product-btn') {
+      if (currCount < valStockCurrProducts) {
+        currCount += 1;
+        countCurrProducts.textContent = `${currCount}`;
+        isPlus = true;
+      }
+    } else if (currBtn.id === 'remove-product-btn') {
+      if (currCount > 1) {
+        currCount--;
+        countCurrProducts.textContent = `${currCount}`;
+      } else {
+        const currBasketPageItem = this.productsItemNumElem[`${id}_block`] as HTMLElement;
+        currBasketPageItem?.remove();
+        //TODO: обновить порядковый номер товара в списке
+        this.updateNumsProductsByOrder();
+      }
+    }
+    //TODO: удалить товар из arrТоваров корзины
+    this.changeProductInBasket(id, isPlus);
+
+    //update data
+    currAmountSum.textContent = `Total: ${price * currCount} €`;
+    //TODO: обновить данные о кол-ве товаров и цены в arrТоваров в Header
+    this.updateHeader();
+    //TODO: обновить данные о кол-ве товаров и цены в arrТоваров в Summary
+    this.summaryData();
+  }
+
+  getProductStockById(id: number): number {
+    const productObj = window.app.dataBase.getProductById(id);
+    return productObj?.stock || 0;
+  }
+
+  changeProductInBasket(id: number, isPlus?: boolean): void {
+    window.app.dataBase.changeAmountInBasket(id, isPlus);
+  }
+
+  updateNumsProductsByOrder(): void {
+    const numElemsCollection = document.querySelectorAll('.basket-page__item-num');
+    numElemsCollection.forEach((numElem, index) => {
+      numElem.textContent = `${index + 1}`;
+    });
+  }
+
+  updateHeader(): void {
+    window.app.header.updateData();
+  }
+
+  basketPageItemTemplate(obj: IProducts, index: number, amount: number): string {
     return `
-    <div class="basket-page__item">
       <div class="basket-page__item-num">${index + 1}</div>
       <div class="basket-page__item-info basket-product-info">
         <div class="basket-product-info__img">
@@ -120,15 +162,14 @@ export default class BasketPage {
         </ul>
       </div>
       <div class="basket-product-info__summary">
-        <div class="basket-product-info__price"><span>Price: </span>${obj.price}&nbsp;$</div>
+        <div class="basket-product-info__price"><span>Price: </span>${obj.price}&nbsp;€</div>
         <div class="basket-product-info__amount-info">
           <button class="basket-product-info__btn btn btn--cart" id="remove-product-btn"> - </button>
-          <span class="basket-product-info__amount amount-of amount-of--dark-bg">1</span>
+          <span class="basket-product-info__amount amount-of amount-of--dark-bg">${amount}</span>
           <button class="basket-product-info__btn btn btn--cart" id="add-product-btn"> + </button>
         </div>
-        <div class="basket-product-info__amount-sum"><span>Total: </span>${obj.price}&nbsp;$</div>
+        <div class="basket-product-info__amount-sum"><span>Total: </span>${obj.price}&nbsp;€</div>
       </div>
-    </div>
     `;
   }
 
@@ -161,10 +202,10 @@ export default class BasketPage {
               <h2 class="basket-summary__title">Summary</h2>
               <div class="basket-summary__products-count"><span>Products: </span>10</div>
               <div class="basket-summary__amount-row">
-                <div class="basket-summary__products-amount" id="products-amount-start"><span>Total: </span>2500.00 $
+                <div class="basket-summary__products-amount" id="products-amount-start"><span>Total: </span>2500.00 €
                 </div>
                 <div class="basket-summary__products-amount promo-amount hide" id="products-amount-promo"><span>Total:
-                  </span>2250.00 $</div>
+                  </span>2250.00 €</div>
               </div>
               <div class="basket-summary__promo-code promo-code">
                 <div class="promo-code__input">
